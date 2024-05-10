25c49b02a53093405063c538d61f46d2a9779261
agarwalmohit43 <agarwalmohit43@gmail.com>
2024-05-10 21:59:40 +0530

    renderItem logged

--- src/screen/List/index.js
+++ src/screen/List/index.js
@@ -2,15 +2,19 @@ import { View, FlatList, StyleSheet } from "react-native";
 import React from "react";
 import ListRow from "./ListRow";
 
+const renderItem = (renderItem) => {
+  //{"index": 0, "item": {"name": "bulbasaur", "url": "https://pokeapi.co/api/v2/pokemon/1/"}, "separators": {"highlight": [Function highlight], "unhighlight": [Function unhighlight], "updateProps": [Function updateProps]}}
+  console.log(renderItem);
+  return <ListRow {...renderItem.item} />;
+};
+
 const List = ({ data }) => {
   return (
     <View style={styles.container}>
       <FlatList
         data={data}
         keyExtractor={(item) => item.name}
-        renderItem={({ item }) => {
-          return <ListRow {...item} />;
-        }}
+        renderItem={renderItem}
       />
     </View>
   );
